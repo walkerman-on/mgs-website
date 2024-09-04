5b1eb3d8a49b5a4345e8d9191f238b2066641cef
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2024-09-04 12:22:00 +0300

    Adding server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./public/db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = 3001;
+server.use(middlewares);
+server.use(router);
+server.listen(port);

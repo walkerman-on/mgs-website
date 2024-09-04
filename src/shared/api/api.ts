4b3806bc0aa938a3beaa07cc3470547408efa585
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2024-09-04 18:03:33 +0300

    Deleting server.js

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const __API__ = '/api'; // Используйте относительный путь
+const __API__ = 'https://mgs-server.vercel.app'; // Используйте относительный путь
 
 export const $api = axios.create({
 	baseURL: __API__,
